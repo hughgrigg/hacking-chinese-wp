533c798ca6ef6c011842c1f9d5f844d3d2cd2ad2
Hugh Grigg <hugh.grigg@gmail.com>
2015-05-31 15:59:09 +0100

    Initial gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
-/**
- * Created by hugh on 15-5-31.
- */
+var
+    gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    minifyCSS   = require('gulp-minify-css'),
+    autoprefix  = require('gulp-autoprefixer');
+
+gulp.task('style', function() {
+    return gulp.src('./scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefix())
+        .pipe(minifyCSS({
+            advanced:            true,
+            aggressiveMerging:   true,
+            keepBreaks:          true,
+            keepSpecialComments: 1
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wp-content/themes/hc-2015'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./themes/2015/scss/**/*.scss', ['style']);
+});
