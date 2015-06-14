d1f0763bb4fb354b0a0003028e1ad761db4956dc
Hugh Grigg <hugh.grigg@gmail.com>
2015-06-14 15:16:37 +0100

    Initial top nav menu

--- wp-content/themes/hc-2015/functions.php
+++ wp-content/themes/hc-2015/functions.php
@@ -1,6 +1,11 @@
 <?php
 
 function hc2015_scripts() {
-	wp_enqueue_style('hc2015-style', get_stylesheet_uri());
+    wp_enqueue_style('hc2015-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'hc2015_scripts');
+
+function register_hc2015_menu() {
+    register_nav_menu('top-nav', __( 'Top Nav' ));
+}
+add_action('init', 'register_hc2015_menu');
--- wp-content/themes/hc-2015/index.php
+++ wp-content/themes/hc-2015/index.php
@@ -1,34 +1,34 @@
 <?php get_header(); ?>
 
-<div class="page">
+<a class="pure-g site-header" href="<?php bloginfo('url'); ?>">
+    <div class="pure-u-1 pure-u-sm-1-6 pure-u-md-1-8">
+        <div class="header-logo icon icon-zhongwen-jiemi"></div>
+    </div>
+    <div class="pure-u-1 pure-u-sm-5-6  pure-u-md-7-8 site-titles">
+        <h2 class="site-title"><?php bloginfo('name'); ?></h2>
+        <h3 class="site-subtitle"><?php bloginfo('description'); ?></h3>
+    </div>
+</a>
 
-    <a class="pure-g site-header" href="<?php bloginfo('url'); ?>">
-        <div class="pure-u-1 pure-u-sm-1-6 pure-u-md-1-8">
-            <div class="header-logo icon icon-zhongwen-jiemi"></div>
-        </div>
-        <div class="pure-u-1 pure-u-sm-5-6  pure-u-md-7-8 site-titles">
-            <h2 class="site-title"><?php bloginfo('name'); ?></h2>
-            <h3 class="site-subtitle"><?php bloginfo('description'); ?></h3>
-        </div>
-    </a>
+<button class="menu-button pure-button">Menu</button>
+<?php wp_nav_menu(array('theme_location' => 'top-nav', 'menu_class' => 'top-menu')) ?>
 
-    <div class="pure-g layout">
+<div class="pure-g layout">
 
-        <div id="content" class="content pure-u-1 pure-u-lg-3-4 pure-u-xl-2-3">
-            <?php
-            // Start the loop.
-            while (have_posts()) : the_post();
-                get_template_part('content', get_post_format());
-            endwhile;
-            ?>
-        </div>
-
-        <aside class="sidebar pure-u-1 pure-u-lg-1-4 pure-u-xl-1-3">
-            <h2>Sidebar</h2>
-        </aside>
-
+    <div id="content" class="content pure-u-1 pure-u-lg-3-4 pure-u-xl-2-3">
+        <?php
+        // Start the loop.
+        while (have_posts()) : the_post();
+            get_template_part('content', get_post_format());
+        endwhile;
+        ?>
     </div>
 
+    <aside class="sidebar pure-u-1 pure-u-lg-1-4 pure-u-xl-1-3">
+        <h2>Sidebar</h2>
+    </aside>
+
 </div>
+
 
 <?php get_footer(); ?>
