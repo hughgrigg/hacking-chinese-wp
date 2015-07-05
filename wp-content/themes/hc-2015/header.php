7b86f9470c175f843fb3ffb9785f93ebcd21e652
Hugh Grigg <hugh.grigg@gmail.com>
2015-07-05 18:59:17 +0100

    Re-arrange font loading

--- wp-content/themes/hc-2015/header.php
+++ wp-content/themes/hc-2015/header.php
@@ -10,6 +10,9 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link href='http://fonts.useso.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
+          rel='stylesheet'
+          type='text/css'>
     <title>
         <?php if (is_front_page()):
             bloginfo('name'); ?> | <?php bloginfo('description');
