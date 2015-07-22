7f9db7801e7372f298f77a117a5a46c2c9527ff4
Hugh Grigg <hugh.grigg@gmail.com>
2015-07-22 21:30:44 +0100

    Address some issues reported by Google Page Speed Insights

--- wp-content/themes/hc-2015/footer.php
+++ wp-content/themes/hc-2015/footer.php
@@ -6,6 +6,9 @@
     <?php wp_footer(); ?>
 </footer>
 
+<link href='http://fonts.useso.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
+      rel='stylesheet'
+      type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
       rel='stylesheet'
       type='text/css'>
