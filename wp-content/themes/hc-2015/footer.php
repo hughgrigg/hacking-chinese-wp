6baf04a9191feab5f4caeeef2071f997230316fa
Hugh Grigg <hugh.grigg@gmail.com>
2016-01-24 13:56:16 +0000

    Make China font server alternative option

--- wp-content/themes/hc-2015/footer.php
+++ wp-content/themes/hc-2015/footer.php
@@ -1,10 +1,12 @@
 <?php wp_footer(); ?>
 
-<link href='http://fonts.useso.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
+      title='Google Fonts Server'
       rel='stylesheet'
       type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
-      rel='stylesheet'
+<link href='http://fonts.useso.com/css?family=Droid+Serif:400,700,400italic|Montserrat'
+      title='China Fonts Server'
+      rel='alternate stylesheet'
       type='text/css'>
 
 </body>
